e5f45770d2144520619b1a47ae2f0a275ab0c623
Jonathan Arenas <jonathanarenas318@gmail.com>
2022-12-10 23:48:08 -0600

    created query and mutation resolvers and the schema grapql for movies and users

--- src/graphql/resolvers/Mutation.js
+++ src/graphql/resolvers/Mutation.js
@@ -1,6 +1,25 @@
+import Movie_catalog from "../../models/Movies_catalog";
+import User from "../../models/User";
+const Mutation = {
+    async createMovie(_, { input }) {
+        const Movies = await Movie_catalog.create(input)
+        return Movies
+    },
+    async updateMovie(_, { input, _id }) {
+        return await Movie_catalog.findByIdAndUpdate(
+            _id,
+            input,
+            { new: true }
+        )
+    },
+    async removeMovie(_, { id }) {
+        await Movie_catalog.findByIdAndRemove(id)
+        return await Movie_catalog.find()
+    },
+    async createUser(_,{input}){
+        const user = await User.create(input)
+        return user
+    }
 
-const Mutation ={
-
-    
 }
 export default Mutation
--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -1,5 +1,24 @@
-import { get } from 'mongoose'
-const Query= {
+import Movie_catalog from '../../models/Movies_catalog'
+import User from '../../models/User'
+const Query = {
+    ping() {
+        return 'pong'
+    },
+    async getMovies() {
+        const Movies = await Movie_catalog.find()
+        return Movies
+    },
+    async getMovieById(_, { _id }) {
+        const Movie = await Movie_catalog.findById(_id)
+        return Movie
+    },
+
+    async login(_, { email, password }) {
+        const verifyUser = await User.find({ email: email, password: password });
+        console.log(verifyUser)
+        return verifyUser
+    }
+
 
 }
 export default Query
