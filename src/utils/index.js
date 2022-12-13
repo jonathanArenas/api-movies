cd87e0a6d8a839d5856fd083588be0719ac7820c
Jonathan Arenas <jonathanarenas318@gmail.com>
2022-12-12 20:51:45 -0600

    created funtion for create toket

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,8 @@
-//jwt secret
+const jtw = require('jsonwebtoken')
+import config from '../config'
+const createToken = (data) =>{
+    const token = jtw.sign(data, config.jwt.secret, {expiresIn: '1d'})
+    return token
+
+}
+module.exports = createToken
